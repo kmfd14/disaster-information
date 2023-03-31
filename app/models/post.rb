da253387f4ab99efb820da0f177bf29f6036374d
kmfd14 <kmfd14@gmail.com>
2023-03-31 15:29:50 +0800

    edited model for user, comments, and image features

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,10 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
-  has_many :comments
+  has_many :comments, dependent: :restrict_with_error
   has_many :post_category_ships
   has_many :categories, through: :post_category_ships
+
+  belongs_to :user
+  mount_uploader :image, ImageUploader
 end
