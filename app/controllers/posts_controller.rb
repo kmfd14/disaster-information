f4072a1112187bfa65d6f79145b3e423f4ebd9f0
kmfd14 <kmfd14@gmail.com>
2023-03-31 12:56:36 +0800

    POSTS: added "address" on the table

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, :address)
   end
 
 end
