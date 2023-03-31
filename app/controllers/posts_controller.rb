accda092e558c8a5fe5d85ec79f5c52cb9cc9d13
kmfd14 <kmfd14@gmail.com>
2023-03-31 15:26:30 +0800

    added user to its dependencies to posts and comments

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,10 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :validate_post_owner, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.includes(:categories, :user).all
   end
 
   def new
@@ -11,6 +13,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
     if @post.save
       redirect_to posts_path
     else
@@ -37,12 +40,18 @@ class PostsController < ApplicationController
 
   private
 
+  def validate_post_owner
+    unless @post.user == current_user
+      flash[:notice] = 'the post not belongs to you'
+      redirect_to posts_path
+    end
+  end
+
   def set_post
     @post = Post.find(params[:id])
   end
 
   def post_params
-    params.require(:post).permit(:title, :content, :address)
+    params.require(:post).permit(:title, :content, :address, :image, category_ids: [])
   end
-
 end
