7af9f4cf1054b3771db4060b0c8a2408bc657b68
kmfd14 <kmfd14@gmail.com>
2023-04-04 13:51:14 +0800

    moved line location for the short_url syntax

--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,11 @@ Rails.application.routes.draw do
   # root "articles#index"
   root "welcome#index"
 
-  get '/posts', to: 'posts#index', as: 'posts'
-  get '/categories', to: 'categories#index', as: 'categories'
-  get '/:short_url', to: 'posts#show', as: 'short_post'
-
   resources :posts do
     resources :comments, except: :show
   end
-  
+
   resources :categories, except: :show
+  get '/:short_url', to: 'posts#show', as: 'short_post'
 
 end
