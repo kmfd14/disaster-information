3efeeb273d94d42f39009dd698981a0df2a9973e
KMFD-LAPTOP\KMFD <kmfd14@gmail.com>
2023-04-13 02:34:52 +0800

    added routes and controller for profile page, edited profile index.html.erb, added a access link to profile page in navbar

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   end
 
   resources :categories, except: :show
+
+  resource :profile, only: [:index], path: ''
+  get '/profile', to: 'profile#index'
+
   get '/:short_url', to: 'posts#show', as: 'short_post'
 
 end
